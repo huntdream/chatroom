2a5f3b734fc6429f2ea87f7c19b41f6d812b1932
huntdream <maoyu960320@hotmail.com>
2018-11-30 18:02:13 +0800

    update

--- src/containers/Index/index.tsx
+++ src/containers/Index/index.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
 import './style.sass';
+import Button from '../../components/Button';
+import Dropdown, { DropdownItem } from '../../components/Dropdown';
 
 class Index extends React.Component {
   render() {
-    return <div className="index">It's My Blog</div>;
+    return (
+      <div className="index">
+        <h2>Build ui components first</h2>
+        <Button color="primary">Primary</Button>
+        <Button icon="close" />
+        <div className="dropdown-wrap">
+          <Dropdown
+            style={{ width: '110px' }}
+            render={<Button>DropDown</Button>}
+          >
+            <DropdownItem>It's me</DropdownItem>
+            <DropdownItem>Dropdown</DropdownItem>
+          </Dropdown>
+        </div>
+      </div>
+    );
   }
 }
 

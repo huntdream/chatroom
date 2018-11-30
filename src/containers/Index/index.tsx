980e7f668c422823f7a21d664839290f52290d9c
huntdream <maoyu960320@hotmail.com>
2018-11-30 15:53:42 +0800

    remove components

--- src/containers/Index/index.tsx
+++ src/containers/Index/index.tsx
@@ -1,29 +1,9 @@
 import * as React from 'react';
-import List from '../List';
-import Message from '../Message';
-import Input from '../Input';
-
 import './style.sass';
 
 class Index extends React.Component {
   render() {
-    return (
-      <div className="index">
-        <div className="left-pane">
-          <div className="list-container">
-            <List />
-          </div>
-        </div>
-        <div className="right-pane">
-          <div className="message-container">
-            <Message />
-          </div>
-          <div className="input-container">
-            <Input />
-          </div>
-        </div>
-      </div>
-    );
+    return <div className="index">It's My Blog</div>;
   }
 }
 

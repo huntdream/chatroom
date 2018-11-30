99c7b423becfce367a92c279b062ded85d7df58d
huntdream <maoyu960320@hotmail.com>
2018-11-30 15:54:03 +0800

    redux initial setup

---
+++ src/redux/reducers/user.js
@@ -0,0 +1,12 @@
+const initialState = {};
+
+const user = (state = initialState, action) => {
+  switch (action.type) {
+    case 'Login':
+      return { ...state };
+    default:
+      return state;
+  }
+};
+
+export default user;

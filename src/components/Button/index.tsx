45d21e90411dcb565b29bf04ce72c358937e4fea
huntdream <maoyu960320@hotmail.com>
2018-11-30 14:21:15 +0800

    add button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import classNames from 'classnames';
+import './style.sass';
+
+export type ButtonType = 'button' | 'submit' | 'reset';
+
+export interface ButtonProps {
+  children?: React.ReactNode;
+  color?: string;
+  icon?: string;
+  prefix?: string;
+  type?: ButtonType;
+}
+
+export type Props = {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+} & ButtonProps;
+
+class Button extends React.Component<Props, any> {
+  static defaultProps = {
+    color: 'primary',
+    prefix: 'btn',
+    type: 'button'
+  };
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      isLoading: false
+    };
+  }
+
+  handleClick: React.MouseEventHandler<HTMLButtonElement> = e => {
+    const { onClick } = this.props;
+    if (onClick) {
+      (onClick as React.MouseEventHandler<HTMLButtonElement>)(e);
+    }
+  };
+
+  render() {
+    const { children, color, icon, prefix, type } = this.props;
+    const classes = classNames(prefix, {
+      [`${prefix}--${color}`]: color,
+      [`${prefix}--icon`]: icon
+    });
+    return (
+      <button type={type} className={classes} onClick={this.handleClick}>
+        <div className="btn-content">
+          {children}
+          {icon && <i className="material-icons">{icon}</i>}
+        </div>
+      </button>
+    );
+  }
+}
+
+export default Button;

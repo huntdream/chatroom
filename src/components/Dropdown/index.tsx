602719ac76a1bfa6209a7c94fe374838ab448535
Yu Mao <maoyu960320@hotmail.com>
2018-12-20 23:35:43 +0800

    update

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -4,33 +4,50 @@ import classNames from 'classnames';
 import './style.sass';
 
 export type DropdownPositions = 'top' | 'right' | 'bottom' | 'left';
+export type TriggerTypes = 'hover' | 'click';
 
 export interface DropdownProps {
   position?: DropdownPositions;
   render: React.ReactNode;
   style?: React.CSSProperties;
+  trigger?: TriggerTypes;
 }
 
 export type Props = {} & DropdownProps;
 
 class Dropdown extends React.Component<Props, any> {
+  static defaultProps = {
+    trigger: 'hover'
+  };
+
   constructor(props: Props) {
     super(props);
     this.state = {
-      isOpen: false
+      isShow: false
     };
   }
+
+  _onClick: React.MouseEventHandler = e => {
+    const { target } = e;
+    const { trigger } = this.props;
+    if (trigger === 'click') {
+      this.setState({
+        isShow: true
+      });
+    }
+  };
+
   render() {
-    const { children, render, style } = this.props;
-    const { isOpen } = this.state;
-    const classes = classNames('dropdown', {
-      [`dropdown--${open}`]: isOpen
+    const { children, render, style, trigger } = this.props;
+    const { isShow } = this.state;
+    const classes = classNames('dropdown', `dropdown__${trigger}`, {
+      show: isShow
     });
     return (
-      <div className={classes}>
+      <div className={classes} onClick={this._onClick}>
         {render}
-        <div className="dropdown-item-wrap" style={style}>
-          {children}
+        <div className="dropdown-item-wrap">
+          <ul className="dropdown-item-list">{children}</ul>
         </div>
       </div>
     );

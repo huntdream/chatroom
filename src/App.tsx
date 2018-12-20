ed3379fd3842fe6a121e7a1b022f557af15da811
Yu Mao <maoyu960320@hotmail.com>
2018-12-20 23:36:59 +0800

    move redux and react-router setup from index.tsx to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
-import * as React from 'react';
-import './App.sass';
-import Index from './containers/Index';
+import * as React from "react";
+import { Provider } from "react-redux";
+import {
+  BrowserRouter as router,
+  Route,
+  Router,
+  Switch
+} from "react-router-dom";
+import createHistory from "history/createBrowserHistory";
+
+import store from "./redux/store";
+
+import "./App.sass";
+import routes from "./routes";
+
+const history = createHistory();
 
 class App extends React.Component {
   render() {
-    return <Index />;
+    return (
+      <Provider store={store}>
+        <Router history={history}>
+          <div className="app-container">
+            <Switch>
+              {routes.map(({ path, name, component: C }) => {
+                return (
+                  <Route
+                    path={path}
+                    key={path}
+                    render={() => (
+                      <React.Suspense fallback={<div>Loading</div>}>
+                        <C />
+                      </React.Suspense>
+                    )}
+                  />
+                );
+              })}
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
+    );
   }
 }
 

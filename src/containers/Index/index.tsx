a0fa5b45513d784217fb6073f5d77bf00a582fbb
Yu Mao <maoyu960320@hotmail.com>
2019-01-07 19:18:22 +0800

    update Index component

--- src/containers/Index/index.tsx
+++ src/containers/Index/index.tsx
@@ -1,26 +1,15 @@
-import * as React from 'react';
-import './style.sass';
-import Button from '../../components/Button';
-import Dropdown, { DropdownItem } from '../../components/Dropdown';
+import * as React from "react";
+import "./style.sass";
+
+import { greeting } from "../../api/hello";
 
 class Index extends React.Component {
+  state = {
+    greeting: "hello"
+  };
+  componentDidMount() {}
   render() {
-    return (
-      <div className="index">
-        <h2>Build ui components first</h2>
-        <Button color="primary">Primary</Button>
-        <Button icon="close" />
-        <div className="dropdown-wrap">
-          <Dropdown
-            style={{ width: '110px' }}
-            render={<Button>DropDown</Button>}
-          >
-            <DropdownItem>It's me</DropdownItem>
-            <DropdownItem>Dropdown</DropdownItem>
-          </Dropdown>
-        </div>
-      </div>
-    );
+    return <div className="index">{this.state.greeting}</div>;
   }
 }
 

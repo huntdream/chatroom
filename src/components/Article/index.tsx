6126a4ec84eaf47f9491e3e4ea01ef18ff38392f
Yu Mao <maoyu960320@hotmail.com>
2019-01-14 21:01:04 +0800

    create Article component

---
+++ src/components/Article/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import "./style.sass";
+
+class Article extends React.Component<any, any> {
+  render() {
+    return <div className="article">article</div>;
+  }
+}
+
+export default Article;

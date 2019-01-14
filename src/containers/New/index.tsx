6e19c8c4f98e1b3371217a8a1f83ef990d579fd2
Yu Mao <maoyu960320@hotmail.com>
2019-01-14 20:58:57 +0800

    create New component

---
+++ src/containers/New/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import MyEditor from "../../components/MyEditor";
+
+class New extends React.Component {
+  render() {
+    return <MyEditor />;
+  }
+}
+
+export default New;

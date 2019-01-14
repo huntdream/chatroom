ee102729c6d448d802d71e94feb122c337f52d7f
Yu Mao <maoyu960320@hotmail.com>
2019-01-14 21:00:56 +0800

    create ArticleLis components

---
+++ src/components/ArticleList/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import "./style.sass";
+
+class ArticleList extends React.Component<any, any> {
+  render() {
+    return <div className="artile-list">articleList</div>;
+  }
+}
+
+export default ArticleList;

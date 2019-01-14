72146fceff7d38b5ac3a452081648ebde2d2216a
Yu Mao <maoyu960320@hotmail.com>
2019-01-14 20:58:28 +0800

    split stylebutton to independent component

---
+++ src/components/MyEditor/StyleButton.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import classnames from "classnames";
+
+const StyleButton = (props: any) => (
+  <button
+    className={classnames("controller-item", {
+      active: props.isInlineStyleActive(props.item.style)
+    })}
+    onMouseDown={event => props.toggleInlineStyle(event, props.item.style)}
+    key={props.item.style}
+  >
+    {props.item.label}
+  </button>
+);
+
+export default StyleButton;

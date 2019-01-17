d31b9c1f4ce2aa27e00106a9690ef04ed47df7e3
Yu Mao <maoyu960320@hotmail.com>
2019-01-17 16:39:52 +0800

    add title and submit

--- src/containers/New/index.tsx
+++ src/containers/New/index.tsx
@@ -1,9 +1,26 @@
-import * as React from "react";
-import MyEditor from "../../components/MyEditor";
+import * as React from 'react';
+import MyEditor from '../../components/MyEditor';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import './style.sass';
 
 class New extends React.Component {
   render() {
-    return <MyEditor />;
+    return (
+      <div className="new-wrap">
+        <div className="new-title">
+          <Input style={{ width: '200px' }} />
+        </div>
+        <div className="new-editor">
+          <MyEditor />
+        </div>
+        <div className="new-controls">
+          <Button color="secondary">Save</Button>
+          <Button>Submit</Button>
+        </div>
+      </div>
+    );
   }
 }
 

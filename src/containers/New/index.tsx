e2f72cd696ba5573f01860df6c7bc1d68e1711cf
Yu Mao <maoyu960320@hotmail.com>
2019-01-21 16:25:49 +0800

    nothing

--- src/containers/New/index.tsx
+++ src/containers/New/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Form from '../../components/Form';
 import MyEditor from '../../components/MyEditor';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -14,22 +15,28 @@ class New extends React.Component {
   render() {
     return (
       <div className="new-wrap">
-        <div className="new-title">
-          <Input
-            label="text"
-            placeholder="Title"
-            style={{ fontSize: '2.4rem' }}
-            defaultValue="Hello"
-          />
-        </div>
-        <div className="new-editor">
-          <MyEditor />
-        </div>
-        <div className="new-controls">
-          <Checkbox label="Private" defaultValue={this.state.checked} />
-          <Button color="secondary">Save</Button>
-          <Button>Submit</Button>
-        </div>
+        <Form>
+          {formState => (
+            <React.Fragment>
+              <div className="new-title">
+                <Input
+                  id="field-title"
+                  label="Title"
+                  placeholder="Title"
+                  style={{ fontSize: '2rem' }}
+                />
+              </div>
+              <div className="new-editor">
+                <MyEditor />
+              </div>
+              <div className="new-controls">
+                <Checkbox label="Private" defaultValue={this.state.checked} />
+                <Button color="secondary">Save</Button>
+                <Button>Submit</Button>
+              </div>
+            </React.Fragment>
+          )}
+        </Form>
       </div>
     );
   }

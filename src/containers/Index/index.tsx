d0b8ed818f13d9047754b593b140600dfc521b6a
Yu Mao <maoyu960320@hotmail.com>
2019-02-05 11:12:55 +0800

    connect with redux

--- src/containers/Index/index.tsx
+++ src/containers/Index/index.tsx
@@ -1,17 +1,40 @@
 import * as React from 'react';
-import ArticleList from '../../components/ArticleList';
+import { connect } from 'react-redux';
+import { allArticle } from '../../redux/actions';
 
+import ArticleList, { ArticlesType } from '../../components/ArticleList';
 import './style.sass';
 
-class Index extends React.Component {
-  componentDidMount() {}
+export interface Props {
+  articlelist: ArticlesType;
+  getarticlelist: any;
+}
+
+interface States {}
+
+class Index extends React.Component<Props, States> {
+  componentDidMount() {
+    this.props.getarticlelist({ page: 1 });
+  }
   render() {
+    const { articlelist } = this.props;
     return (
       <div className="index">
-        <ArticleList />
+        <ArticleList articlelist={articlelist} />
       </div>
     );
   }
 }
 
-export default Index;
+const mapStateToProps = ({ articles }: { articles: any }) => ({
+  articlelist: articles.list
+});
+
+const mapDipsatchToProps = (dispatch: any) => ({
+  getarticlelist: (payload: any) => dispatch(allArticle.request(payload))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDipsatchToProps
+)(Index);

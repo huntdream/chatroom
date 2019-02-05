9f41cc190bc555fdc289210112c92e39feef497c
Yu Mao <maoyu960320@hotmail.com>
2019-02-05 11:13:55 +0800

    updat route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,13 +14,22 @@ const routes = [
     path: '/',
     name: 'Index',
     exact: true,
+    nav: true,
     component: lazy(() => import('../containers/Index/index'))
   },
   {
     path: '/new',
     name: 'New',
     exact: true,
+    nav: true,
     component: lazy(() => import('../containers/New'))
+  },
+  {
+    path: '/article/:link',
+    name: 'Page',
+    exact: false,
+    nav: false,
+    component: lazy(() => import('../containers/Page'))
   }
   /**
    * {

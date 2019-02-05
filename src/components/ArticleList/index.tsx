32869adcaecb47e32a4505612a06568b75530333
Yu Mao <maoyu960320@hotmail.com>
2019-02-05 11:12:20 +0800

    update ArticleList

--- src/components/ArticleList/index.tsx
+++ src/components/ArticleList/index.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
-import { connect } from 'react-redux';
-import { getArticleList } from '../../redux/actions';
-import { Dispatch } from 'redux';
-import Action from '../../redux/actions/types';
 
-import Article from '../Article';
+import ArticleItem from '../ArticleItem';
 
 import './style.sass';
 
@@ -14,6 +10,7 @@ export interface ArticleType {
   date?: string;
   content: string;
   id: string;
+  suffixCode: string;
 }
 
 export interface ArticlesType extends Array<ArticleType> {
@@ -22,35 +19,19 @@ export interface ArticlesType extends Array<ArticleType> {
 
 export interface Props {
   articlelist: ArticlesType;
-  getarticlelist: any;
 }
 
 class ArticleList extends React.Component<Props, any> {
-  componentDidMount() {
-    this.props.getarticlelist({ page: 1 });
-  }
-
   render() {
     const { articlelist } = this.props;
     return (
       <div className="artile-list">
         {articlelist.map((article: ArticleType, index: number) => (
-          <Article article={article} key={index} />
+          <ArticleItem article={article} key={index} />
         ))}
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ article }: { article: any }) => ({
-  articlelist: article.list
-});
-
-const mapDipsatchToProps = (dispatch: any) => ({
-  getarticlelist: (params: any) => dispatch(getArticleList(params))
-});
-
-export default connect(
-  mapStateToProps,
-  mapDipsatchToProps
-)(ArticleList);
+export default ArticleList;

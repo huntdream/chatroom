ae59b86ef1f15b0df9511d0ad035303bc33cb22f
Yu Mao <maoyu960320@hotmail.com>
2019-02-05 11:13:47 +0800

    sagas

--- src/redux/sagas/index.tsx
+++ src/redux/sagas/index.tsx
@@ -1,6 +1,38 @@
-import { all } from 'redux-saga/effects';
-import * as article from './article';
+import api from '../../api';
+import * as actions from '../actions';
+import {
+  call,
+  put,
+  take,
+  fork,
+  all,
+  takeEvery
+} from '@redux-saga/core/effects';
 
-export default function* rootSagas() {
-  yield all([article.getArticleList({ type: 'GET_ARTICLE_LIST' })]);
+function* fetchEntity(type: any, apiFn: any, payload: any) {
+  const response = yield call(apiFn, payload);
+  if (response) {
+    yield put(type.success(response));
+  } else {
+    put(type.failure('error'));
+  }
+}
+
+function* getAllArticle(action: any) {
+  yield fetchEntity(
+    actions.allArticle,
+    api.article.getArticleList,
+    action.payload
+  );
+}
+
+function* getArticle(action: any) {
+  yield fetchEntity(actions.article, api.article.getArticle, action.payload);
+}
+
+export default function* rootSaga() {
+  yield all([
+    takeEvery(actions.ALL_ARTICLE.REQUEST, getAllArticle),
+    takeEvery(actions.ARTICLE.REQUEST, getArticle)
+  ]);
 }

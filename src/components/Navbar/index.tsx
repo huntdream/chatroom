3fb96851ae74d9182d8644318dcaa1a22347b4b1
Yu Mao <maoyu960320@hotmail.com>
2019-02-05 11:12:38 +0800

    add filter to navlinks

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,17 +1,19 @@
-import * as React from "react";
-import { NavLink } from "react-router-dom";
-import routes from "../../routes";
+import * as React from 'react';
+import { NavLink } from 'react-router-dom';
+import routes from '../../routes';
 
-import "./style.sass";
+import './style.sass';
 
 class Navbar extends React.Component<any, any> {
   // render Navlinks
   renderLinks = () =>
-    routes.map(link => (
-      <NavLink to={link.path} key={link.path} className="nav-link">
-        {link.name}
-      </NavLink>
-    ));
+    routes
+      .filter(link => link.nav)
+      .map(link => (
+        <NavLink to={link.path} key={link.path} className="nav-link">
+          {link.name}
+        </NavLink>
+      ));
 
   render() {
     return (

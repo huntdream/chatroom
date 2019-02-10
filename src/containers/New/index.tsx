d0a277a785850911a5c34040658700758b421d37
Yu Mao <maoyu960320@hotmail.com>
2019-02-10 14:03:38 +0800

    update New

--- src/containers/New/index.tsx
+++ src/containers/New/index.tsx
@@ -1,45 +1,84 @@
 import * as React from 'react';
 import Form from '../../components/Form';
+import { connect } from 'react-redux';
 import MyEditor from '../../components/MyEditor';
 import Button from '../../components/Button';
-import Input from '../../components/Input';
+import Input, { ChangeEventHanlder } from '../../components/Input';
 import Checkbox from '../../components/Checkbox';
+
+import { addArticle } from '../../redux/actions';
 
 import './style.sass';
 
-class New extends React.Component {
+export interface Props {
+  addarticle: any;
+}
+
+interface States {
+  title: string;
+  content: string;
+}
+
+class New extends React.Component<Props, States> {
   state = {
-    checked: true
+    checked: true,
+    title: '',
+    content: ''
+  };
+
+  setTitle: ChangeEventHanlder = e => {
+    this.setState({
+      title: e.target.value
+    });
+  };
+
+  setContent = (content: string) => {
+    this.setState({
+      content
+    });
+  };
+
+  onSubmit = () => {
+    const { title, content } = this.state;
+    this.props.addarticle({ title, content });
   };
 
   render() {
     return (
       <div className="new-wrap">
-        <Form>
+        {/* <Form>
           {formState => (
-            <React.Fragment>
-              <div className="new-title">
-                <Input
-                  id="field-title"
-                  label="Title"
-                  placeholder="Title"
-                  style={{ fontSize: '2rem' }}
-                />
-              </div>
-              <div className="new-editor">
-                <MyEditor />
-              </div>
-              <div className="new-controls">
-                <Checkbox label="Private" defaultValue={this.state.checked} />
-                <Button color="secondary">Save</Button>
-                <Button>Submit</Button>
-              </div>
-            </React.Fragment>
+            <React.Fragment> */}
+        <div className="new-title">
+          <Input
+            autocomplete="off"
+            id="field-title"
+            label="Title"
+            style={{ fontSize: '2rem' }}
+            onChange={this.setTitle}
+          />
+        </div>
+        <div className="new-editor">
+          <MyEditor setContent={this.setContent} />
+        </div>
+        <div className="new-controls">
+          <Checkbox label="Private" defaultValue={this.state.checked} />
+          <Button color="secondary">Save</Button>
+          <Button onClick={this.onSubmit}>Publish</Button>
+        </div>
+        {/* </React.Fragment>
           )}
-        </Form>
+        </Form> */}
       </div>
     );
   }
 }
 
-export default New;
+const mapDisaptchToProps = (dispatch: any) => ({
+  addarticle: (payload: any) => dispatch(addArticle.request(payload))
+});
+
+export default connect(
+  null,
+  mapDisaptchToProps
+)(New);

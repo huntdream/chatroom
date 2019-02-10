6e8db44112c6c77a9c5e42f21826f6a84e66811a
Yu Mao <maoyu960320@hotmail.com>
2019-02-10 13:49:40 +0800

    update api

--- src/api/article.tsx
+++ src/api/article.tsx
@@ -11,3 +11,11 @@ export const getArticleList = (params: any): any => {
 export const getArticle = (params: any): any => {
   return request(`/article/query?${params.key}=${params.value}`);
 };
+
+// ADD an Article
+export const addArticle = (params: any): any => {
+  return request('/article/add', {
+    method: 'POST',
+    data: params
+  });
+};
